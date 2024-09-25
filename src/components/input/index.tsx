09f711ffaa97a1f7e176fa3b5fec6a000d64634e
fabiosena1436 <128174066+fabiosena1436@users.noreply.github.com>
2024-09-25 14:56:25 -0300

    Componente Input e Máscara

---
+++ src/components/input/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from "./styles";
+
+
+
+export function Input(){
+    return(
+        <Container>
+            <label></label>
+            <input type="text" />
+        </Container>
+    )
+}
--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,4 +1,5 @@
 import { Button } from "../components/button";
+import { Input } from "../components/input";
 import { Logo } from "../components/logo";
 import { Title } from "../components/title";
 import { Filters, Header, Main, Section } from "./styles";
@@ -6,21 +7,24 @@ import { Filters, Header, Main, Section } from "./styles";
 export function Home() {
     return (
         <>
-        <Header>
-            <Logo />
-            <div>
-            <Button> Nova transação </Button >
-            <Button> Nova categoria </Button >
-            </div>
-        </Header>
+            <Header>
+                <Logo />
+                <div>
+                    <Button> Nova transação </Button >
+                    <Button> Nova categoria </Button >
+                </div>
+            </Header>
 
-        <Main>
-        <Section>
-            <Filters>
-                <Title title="Saldo" subtitle="Receitas e despesas no perildo"/>
-            </Filters>
-        </Section>
-        </Main>
+            <Main>
+                <Section>
+                    <Filters>
+                        <Title title="Saldo" subtitle="Receitas e despesas no perildo" />
+                        <div>
+                            <Input />
+                        </div>
+                    </Filters>
+                </Section>
+            </Main>
         </>
 
     )
