f8aec2738f556469587815d072eccea16cdae677
fabiosena1436 <128174066+fabiosena1436@users.noreply.github.com>
2024-09-26 17:15:27 -0300

    Componente Card

---
+++ src/components/card/index.tsx
@@ -0,0 +1,26 @@
+import { ArrowCircleDownRight, ArrowCircleUpRight, CurrencyCircleDollar } from "@phosphor-icons/react";
+import { Container } from "./styles";
+import { formatCurrency } from "../../utils/format-currency";
+
+
+type CardProps = {
+    variant?: 'balance' | 'incomes' | 'expenses'
+    title: string
+    amount: number
+}
+
+const iconsMap = {
+    balance: <CurrencyCircleDollar />,
+    incomes: <ArrowCircleUpRight />,
+    expenses: <ArrowCircleDownRight />,
+}
+
+export function Card({ amount, title, variant = 'balance' }: CardProps) {
+    return (
+        <Container $variant={variant}>
+            {iconsMap[variant]}
+            <span>{title}</span>
+            <strong>{formatCurrency(amount)}</strong>
+        </Container>
+    )
+}
